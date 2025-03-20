8ca627140c75bfd7625e976a7dcb8cb0adb3376f
JEVizcaya <jevizcaya@hotmail.com>
2025-03-20 20:11:51 +0100

    ver y realizar solicitudes fincionando

--- prestamos/admin.py
+++ prestamos/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Prestamo, Estado
+
+
+class EstadoAdmin(admin.ModelAdmin):
+    list_display = ('estado_id', 'descripcion')
+    list_display_links = ('estado_id',)
+
 admin.site.register(Prestamo)
-admin.site.register(Estado)
+admin.site.register(Estado,EstadoAdmin)
 
 # Register your models here.
