476b69c27f71cd1018ac134e789219812c2d59a6
JEVizcaya <jevizcaya@hotmail.com>
2025-03-21 20:08:47 +0100

    aceptar y rechazar ok

--- prestamos/views.py
+++ prestamos/views.py
@@ -5,9 +5,10 @@ from .models import Libro,Prestamo
 def index(request):
     if(request.session.get('usuario_id')): 
         #obtengo los libros que no son del usuario logeado y excluir los libros prestados (estado=3)
-        libros_disponibles = Libro.objects.exclude(usuario_id=request.session.get('usuario_id')).exclude(libro_id__in=Prestamo.objects.filter(estado_id=4).values('libro_id'))           
-        
-        return render(request, 'prestamos/index.html', {'libros': libros_disponibles}) 
+        libros_disponibles = Libro.objects.exclude(usuario_id=request.session.get('usuario_id')).exclude(libro_id__in=Prestamo.objects.filter(estado_id=3).values('libro_id'))           
+        #obtengo los libros que he solicitado
+        solicitudes_prestamos = Prestamo.objects.filter(usuario_id=request.session.get('usuario_id'))
+        return render(request, 'prestamos/index.html', {'libros': libros_disponibles, 'solicitudes_prestamos': solicitudes_prestamos}) 
     else:
         #redirijo a la pagina de login
         return redirect('usuarios:login')
@@ -19,4 +20,16 @@ def solicitar(request, libro_id):
         return redirect('prestamos:index')
     prestamo = Prestamo(libro=libro, usuario_id=request.session.get('usuario_id'), estado_id=1)
     prestamo.save()
-    return redirect('prestamos:index')  # Redirige a la lista de libros, por ejemplo.  
+    return redirect('prestamos:index')  # Redirige a la lista de libros, por ejemplo. 
+ 
+def aceptar(request, prestamos_id):
+    prestamo = Prestamo.objects.get(prestamos_id=prestamos_id,estado_id=1)
+    prestamo.estado_id = 4
+    prestamo.save()
+    return redirect('prestamos:index')  # Redirige a la lista de libros, por ejemplo.
+
+def cancelar(request, prestamos_id):
+    prestamo = Prestamo.objects.get(prestamos_id=prestamos_id,estado_id=1)
+    prestamo.estado_id = 3
+    prestamo.save()
+    return redirect('prestamos:index')  # Redirige a la lista de libros, por ejemplo.
